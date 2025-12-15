f035e5f1453f56a973b5f7141d68df3ca4c9d37b
eldadev <eldayia@gmail.com>
2025-12-15 16:35:12 +0100

    Simplify runtime-directory module to only enable logind

--- nixos/modules/runtime-directory.nix
+++ nixos/modules/runtime-directory.nix
@@ -4,13 +4,8 @@
   # --- CONFIGURATION XDG_RUNTIME_DIR ---
   # Nécessaire pour Wayland/Hyprland
 
-  # Activer la gestion automatique des répertoires runtime
-  services.logind = {
-    extraConfig = "RuntimeDirectorySize=1G";
-  };
+  # systemd-logind crée automatiquement /run/user/$UID sur NixOS
+  # Ce module assure juste que le service est activé
 
-  # Créer le répertoire au démarrage si nécessaire
-  systemd.tmpfiles.rules = [
-    "d /run/user 0755 root root -"
-  ];
+  services.logind.enable = true;
 }

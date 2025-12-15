106a675570b08886e0f6e07ebee1c8a555e21104
eldadev <eldayia@gmail.com>
2025-12-15 18:34:23 +0100

    Add X11 fonts for xterm compatibility

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -6,5 +6,13 @@
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     font-awesome
+    # Polices X11 de base pour xterm
+    xorg.fontmiscmisc
+    xorg.fontcursormisc
   ];
+
+  # Configuration polices par défaut
+  fonts.fontconfig.defaultFonts = {
+    monospace = [ "JetBrainsMono Nerd Font" "DejaVu Sans Mono" ];
+  };
 }
